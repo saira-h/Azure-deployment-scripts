37dca7978dea9619c7c608887a678080a8efe098
Saira Hassan <30358249+saira-h@users.noreply.github.com>
2020-08-11 06:29:25 -0700

    Prepare VM for upload to Azure

---
+++ preparevm.ps1
@@ -0,0 +1,76 @@
+#Remove any winHTTP proxy
+netsh.exe winhttp reset proxy
+
+#set SAN policy to onlineAll
+diskpart.exe /s values.txt
+
+#Set UTC for Windows, and start time service automatically
+Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation -Name RealTimeIsUniversal -Value 1 -Type DWord -Force
+Set-Service -Name w32time -StartupType Automatic
+
+#set power profile to high performance
+powercfg.exe /setactive SCHEME_MIN
+
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment' -Name TEMP -Value "%SystemRoot%\TEMP" -Type ExpandString -Force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment' -Name TMP -Value "%SystemRoot%\TEMP" -Type ExpandString -Force
+
+Get-Service -Name BFE, Dhcp, Dnscache, IKEEXT, iphlpsvc, nsi, mpssvc, RemoteRegistry |
+  Where-Object StartType -ne Automatic |
+    Set-Service -StartupType Automatic
+
+Get-Service -Name Netlogon, Netman, TermService |
+  Where-Object StartType -ne Manual |
+    Set-Service -StartupType Manual
+	
+#enable rdp
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server' -Name fDenyTSConnections -Value 0 -Type DWord -Force
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -Name fDenyTSConnections -Value 0 -Type DWord -Force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -Name PortNumber -Value 3389 -Type DWord -Force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -Name LanAdapter -Value 0 -Type DWord -Force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name UserAuthentication -Value 1 -Type DWord -Force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name SecurityLayer -Value 1 -Type DWord -Force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name fAllowSecProtocolNegotiation -Value 1 -Type DWord -Force
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -Name KeepAliveEnable -Value 1  -Type DWord -Force
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -Name KeepAliveInterval -Value 1  -Type DWord -Force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -Name KeepAliveTimeout -Value 1 -Type DWord -Force
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -Name fDisableAutoReconnect -Value 0 -Type DWord -Force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -Name fInheritReconnectSame -Value 1 -Type DWord -Force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -Name fReconnectSame -Value 0 -Type DWord -Force
+if ((Get-Item -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp').Property -contains 'SSLCertificateSHA1Hash')
+{
+    Remove-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name SSLCertificateSHA1Hash -Force
+}
+
+Set-NetFirewallRule -DisplayGroup 'Remote Desktop' -Enabled True
+netstat.exe -anob
+
+#configure windows firewall rule and winrm
+Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled True
+Enable-PSRemoting -SkipNetworkProfileCheck -Force
+Set-NetFirewallRule -DisplayName 'Windows Remote Management (HTTP-In)' -Enabled True
+
+Set-NetFirewallRule -DisplayName 'File and Printer Sharing (Echo Request - ICMPv4-In)' -Enabled True
+
+#allow azure access to vm 
+New-NetFirewallRule -DisplayName AzurePlatform -Direction Inbound -RemoteAddress 168.63.129.16 -Profile Any -Action Allow -EdgeTraversalPolicy Allow
+New-NetFirewallRule -DisplayName AzurePlatform -Direction Outbound -RemoteAddress 168.63.129.16 -Profile Any -Action Allow
+
+#make sure disk is consistent
+chkdsk.exe /f
+
+bcdedit.exe /set "{bootmgr}" integrityservices enable
+bcdedit.exe /set "{default}" device partition=C:
+bcdedit.exe /set "{default}" integrityservices enable
+bcdedit.exe /set "{default}" recoveryenabled Off
+bcdedit.exe /set "{default}" osdevice partition=C:
+bcdedit.exe /set "{default}" bootstatuspolicy IgnoreAllFailures
+
+#Enable Serial Console Feature
+bcdedit.exe /set "{bootmgr}" displaybootmenu yes
+bcdedit.exe /set "{bootmgr}" timeout 5
+bcdedit.exe /set "{bootmgr}" bootems yes
+bcdedit.exe /ems "{current}" ON
+bcdedit.exe /emssettings EMSPORT:1 EMSBAUDRATE:115200
+
+#verify WMI repo is consistent
+winmgmt.exe /verifyrepository
